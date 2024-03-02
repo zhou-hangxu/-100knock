c29167b442085970c46294dfc259aca1f44e1405
zhou-hangxu <131327688+zhou-hangxu@users.noreply.github.com>
2024-03-02 10:23:08 +0900

    Update knock01.py

--- chapter01/knock01.py
+++ chapter01/knock01.py
@@ -1,14 +1,5 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 a='パタトクカシーー'
 print(a[0],a[2],a[4],a[6])
 パ ト カ ー
 
 print('パトカー’＋'タクシー')
-      
-SyntaxError: unterminated string literal (detected at line 1)
-print('パトカー’＋'タクシー')
-      
-SyntaxError: unterminated string literal (detected at line 1)
-print('パトカー’＋')
-      
